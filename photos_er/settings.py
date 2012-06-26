ebdcab0c523a5a2fadd0bdb366e2347bc131f2e7
Edgar Roman <emroman@pbs.org>
2012-06-25 23:24:29 -0400

    Added thumbnail generation

--- photos_er/settings.py
+++ photos_er/settings.py
@@ -128,6 +128,8 @@ INSTALLED_APPS = (
     'django.contrib.humanize',
     # schema migration
     'south',
+    # thumbnail generation
+    'sorl.thumbnail',
 )
 
 # A sample logging configuration. The only tangible logging
