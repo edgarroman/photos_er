ff8230a9c30d887c64e650f48460912ed6716fba
Edgar Roman <emroman@pbs.org>
2012-06-26 22:54:11 -0400

    messing with paths

--- photos_er/settings_local_sample.py
+++ photos_er/settings_local_sample.py
@@ -35,6 +35,12 @@ BRAND_GA_CODE = ''
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = '/home/user/site.com/photos_er/media/'
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = '/home/user/site.com/photos_er/static/'
+
 # Make this unique, and don't share it with anybody.
 # generate this using: http://www.miniwebtool.com/django-secret-key-generator/
 SECRET_KEY = ''
