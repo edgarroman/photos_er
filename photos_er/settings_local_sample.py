dd357e0988a72745a74fbfa62aaf8e4213548d08
Edgar Roman <emroman@pbs.org>
2012-07-06 12:41:55 -0400

    added ability to brand site

--- photos_er/settings_local_sample.py
+++ photos_er/settings_local_sample.py
@@ -34,16 +34,20 @@ BRAND_GA_CODE = ''
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = '/home/user/site.com/photos_er/media/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = '/home/user/site.com/photos_er/static/'
+STATIC_URL = '/static/'
 
 # Make this unique, and don't share it with anybody.
 # generate this using: http://www.miniwebtool.com/django-secret-key-generator/
 SECRET_KEY = ''
 
 TEMP_DIRECTORY = '/home/user/site.com/photos_er/tmp/'
+
+SITE_NAME = 'My photo site'
 
