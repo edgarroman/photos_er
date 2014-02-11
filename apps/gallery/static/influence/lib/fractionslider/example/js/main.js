bf94d14ab9cccc63d9fb49c5c03b95f3d11d0cf1
Edgar Roman <emroman@pbs.org>
2014-02-10 23:22:40 -0500

    Added new website theme - album view now is masonry layout

---
+++ apps/gallery/static/influence/lib/fractionslider/example/js/main.js
@@ -0,0 +1,12 @@
+$(window).load(function(){
+	$('.slider').fractionSlider({
+		'fullWidth': 			true,
+		'controls': 			true, 
+		'pager': 				true,
+		'responsive': 			true,
+		'dimensions': 			"1000,400",
+	    'increase': 			false,
+		'pauseOnHover': 		true
+	});
+
+});
