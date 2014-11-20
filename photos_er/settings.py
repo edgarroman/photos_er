42ad2aeafb22d895aa82f96bda0215b362e0309b
Edgar Roman <emroman@pbs.org>
2014-11-20 13:02:54 -0500

    Added custom error templates

--- photos_er/settings.py
+++ photos_er/settings.py
@@ -110,10 +110,14 @@ ROOT_URLCONF = 'photos_er.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'photos_er.wsgi.application'
 
+import os
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__)) + '/../'
+print ('SITE_ROOT = %s' % SITE_ROOT)
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
