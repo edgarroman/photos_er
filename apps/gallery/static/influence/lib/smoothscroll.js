925cea560989544c86e8095a86e2a0ca3dbdd4e4
Edgar Roman <emroman@pbs.org>
2014-11-20 12:49:06 -0500

    removed unused theme

--- apps/gallery/static/influence/lib/smoothscroll.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("a[href*='#']").click(function(){
-        $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
-        }, 500);
-        return false;
-    });
-});
